818162d2579f3a39bfa9b48c7fbc3cf23514b70e
abedag <abedalrahman7117@gmail.com>
2025-04-16 00:51:49 +0300

    Add <logic.js> Adding an argumeent for the function attribute (inputs)

--- scripts/logic.js
+++ scripts/logic.js
@@ -18,3 +18,11 @@ function generateReports(students) {
   })
 }
 
+const students = [
+  { name: "Alice", scores: [90, 85, 92] },
+  { name: "Bob", scores: [70, 68, 72] },
+  { name: "Charlie", scores: [100, 100, 100] }
+]
+
+const display = generateReports(students);
+
