1fda8b3b45ecc0cae9a66242d3693b2972ffbd0e
abedag <abedalrahman7117@gmail.com>
2025-04-16 03:48:50 +0300

    Add <script.js> Adding some descriptive comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
-const input = document.querySelector(".task-input");
-const button = document.querySelector(".add-btn");
-const list = document.querySelector(".task-list");
+const input = document.querySelector(".task-input"); // Text box
+const button = document.querySelector(".add-btn");   // Add Task button
+const list = document.querySelector(".task-list");   // <ul> where tasks go
 
 button.addEventListener("click", function () {
   const taskText = input.value
 
-  if (taskText === "") return;
+  if (taskText === "") return; // Do nothing if input is empty
 
   const li = document.createElement("li");
-  li.textContent = taskText;
+  li.textContent = taskText; // Add the task text to the <li>
 
   li.addEventListener("click", function () {
-    li.classList.toggle("completed");
+    li.classList.toggle("completed"); // Toggle the 'completed' style
   });
 
-  list.appendChild(li);
+  list.appendChild(li); // Add <li> to list
 
-  input.value = "";
+  input.value = ""; // Clear input box
 })
