0c7a4f48fdf207d42d4688bb484ac198209c807f
abedag <abedalrahman7117@gmail.com>
2025-04-16 03:39:41 +0300

    Add <script.js> Adding a fuction containing variable assigned with task input (also a condition if text is empty)

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 const input = document.querySelector(".task-input");
 const button = document.querySelector(".add-btn");
 const list = document.querySelector(".task-list");
+
+button.addEventListener("click", function () {
+  const taskText = input.value
+
+  if (taskText === "") return;
+
+
+})
